80bbf0d80c2e6cd06763234de1c58719efe9313b
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2025-11-02 11:49:51 +0530

    Set favicon icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/img/loader.png" type="image/png">
+        <link rel="apple-touch-icon" href="/img/loader.png">
 
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
